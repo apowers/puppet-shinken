e34656229ee5e24ea982a7ac59f1a08fce1d2708
Atom Powers <atom.powers@seattlebiomed.org>
2014-08-06 11:43:04 -0700

    templates are special

--- manifests/objects/template.pp
+++ manifests/objects/template.pp
@@ -1,16 +1,12 @@
 # Create a Shinken Module
-define shinken::config::object (
+define shinken::objects::template (
   $ensure       = 'present',
   $object_type  = undef,
   $options      = {},
-  $object_path  = undef,
 ) {
-  if $object_path {
-    $resource_path = $object_path
-  } else {
-    $resource_path  = "${::shinken::config_path}/${object_type}s"
-  }
+  $resource_path  = "${::shinken::config_path}/templates"
 
+  validate_string($object_type)
   validate_absolute_path($resource_path)
   validate_hash($options)
 
